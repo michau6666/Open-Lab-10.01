2c92e3c13779841555e061390c13290584362269
Michal Bernát <michal.bernat.l312@gmail.com>
2022-11-22 20:41:34 +0100

    finished tasks in quest 10.01

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,10 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Book LOTR = new Book();
+            LOTR.title = "Lord Of The Rings";
+            Console.Write(LOTR.title);
+            Console.ReadKey();
         }
     }
 }
